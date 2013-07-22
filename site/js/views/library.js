1582770b2edd257969c7995c5e59be9baa23a074
Jean-Christophe Moinet <jean-christophe.moinet@valtech.fr>
2013-07-22 12:01:49 +0200

    added node server

--- site/js/views/library.js
+++ site/js/views/library.js
@@ -5,6 +5,7 @@ app.LibraryView = Backbone.View.extend({
 
     initialize: function( initialBooks ) {
         this.collection = new app.Library( initialBooks );
+        this.listenTo( this.collection, 'add', this.renderBook );
         this.render();
     },
 
@@ -22,5 +23,24 @@ app.LibraryView = Backbone.View.extend({
             model: item
         });
         this.$el.append( bookView.render().el );
-    }
+    },
+    
+    events:{
+        'click #add':'addBook'
+    },
+
+    addBook: function( e ) {
+        e.preventDefault();
+
+        var formData = {};
+
+        $( '#addBook div' ).children( 'input' ).each( function( i, el ) {
+            if( $( el ).val() != '' )
+            {
+                formData[ el.id ] = $( el ).val();
+            }
+        });
+
+        this.collection.add( new app.Book( formData ) );
+    },
 });
