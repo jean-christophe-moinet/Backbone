437b4e26217ce9fdd0c9243cd8bcdb7adb90e1c2
Jean-Christophe Moinet <jean-christophe.moinet@valtech.fr>
2013-07-22 21:22:56 +0200

    improvements

--- site/js/views/library.js
+++ site/js/views/library.js
@@ -34,17 +34,29 @@ app.LibraryView = Backbone.View.extend({
 		'click #add' : 'addBook'
 	},
 
-	addBook : function(e) {
-		e.preventDefault();
+	addBook: function( e ) {
+	    e.preventDefault();
 
-		var formData = {};
+	    var formData = {};
 
-		$('#addBook div').children('input').each(function(i, el) {
-			if ($(el).val() != '') {
-				formData[el.id] = $(el).val();
-			}
-		});
+	    $( '#addBook div' ).children( 'input' ).each( function( i, el ) {
+	        if( $( el ).val() != '' )
+	        {
+	            if( el.id === 'keywords' ) {
+	                formData[ el.id ] = [];
+	                _.each( $( el ).val().split( ' ' ), function( keyword ) {
+	                    formData[ el.id ].push({ 'keyword': keyword });
+	                });
+	            } else if( el.id === 'releaseDate' ) {
+	                formData[ el.id ] = $( '#releaseDate' ).datepicker( 'getDate' ).getTime();
+	            } else {
+	                formData[ el.id ] = $( el ).val();
+	            }
+	        }
+	        // Clear input field value
+	        $( el ).val('');
+	    });
 
-		this.collection.add(new app.Book(formData));
+	    this.collection.create( formData );
 	},
 });
